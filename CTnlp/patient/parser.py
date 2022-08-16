1edae6fd3c956aceb6ba37225fff92914e1db8a6
Wojciech Kusa <WojciechKusa@users.noreply.github.com>
2022-08-16 13:14:23 +0200

    feat(patient): add loaders of patient data (#13)
    * feat(patient): add loaders of patient data
    
    * Update tests/unit/test_patients_loader.py
    
    Co-authored-by: code-review-doctor[bot] <72320148+code-review-doctor[bot]@users.noreply.github.com>
    
    * 'Refactored by Sourcery' (#14)
    
    Co-authored-by: Sourcery AI <>
    
    Co-authored-by: code-review-doctor[bot] <72320148+code-review-doctor[bot]@users.noreply.github.com>
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

---
+++ CTnlp/patient/parser.py
@@ -0,0 +1,44 @@
+from typing import List
+
+import defusedxml.ElementTree as ET
+
+from CTnlp.patient.patient import Patient
+
+
+def load_patients_from_xml(
+    patient_file: str, input_type: str = "TREC"
+) -> List[Patient]:
+    """Parses patients data from a single XML file and creates a Patient class instance
+     for each parsed item.
+
+    :param patient_file: str
+    :param input_type: str describes the type of parser that should be used.
+    Currently only two types of xml files are supported: TREC-style and CSIRO-style.
+    :return: List of patients
+    """
+    tree = ET.parse(patient_file)
+    root = tree.getroot()
+
+    patients = []
+    if input_type == "CSIRO":
+        patients.extend(
+            Patient(
+                patient_id=int(elem.attrib["number"]),
+                description=elem[0].text.strip(),
+            )
+            for elem in root
+        )
+
+    elif input_type == "TREC":
+        patients.extend(
+            Patient(
+                patient_id=int(elem.attrib["number"]),
+                description=elem.text.strip(),
+            )
+            for elem in root
+        )
+
+    else:
+        raise ValueError("input_type can be only TREC or CSIRO")
+
+    return patients
